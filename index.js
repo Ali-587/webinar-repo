1106d58ab79db18a0a707a90c1dd882c80c0bf5e
Ali-587 <alimcm587@gmail.com>
2022-07-19 15:49:45 +0500

    First Change

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var express = require('express');
  
 var app = express();
-var calc = 2;
+var calc = 5;
 app.get('/', function (req, res) {
- res.send('Welcome To The Webinar Web Application ! Thanks for taking part in this Video :-)' + "</br>" + 'The result of 1+1 is: ' + calc);
+ res.send('Welcome To The Personal  Webinar Web Application ! Thanks for taking part in this Video :-)' + "</br>" + 'The result of 1+1 is: ' + calc);
 });
 app.listen(process.env.PORT || 3000);
  
